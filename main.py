ca7522ddf0d0facc715b8cafa8faf47ed7c88031
Aidanhubley <ah1632@messiah.edu>
2023-02-01 19:20:43 -0500

    All Scrolling Works. 3 Canvas = 3 Scrolling

--- main.py
+++ main.py
@@ -45,11 +45,27 @@ def open_review():
     review.title("Review Page")
     review.geometry("500x700")
 
-    # Buttons
+    # Buttons + Label
     reselect = Button(review, text="< Reselect Groups", command=grouping_page_rerun)
     reselect.place(relx=.05, rely=.04, anchor=NW)
     save_groups = Button(review, text="Save Groups >", command=open_final)
     save_groups.place(relx=.95, rely=.04, anchor=NE)
+    grouped_label = Label(review, text="Review Groups", font=(20))
+    grouped_label.place(relx=.5, rely=.1, anchor=CENTER)
+
+    # Canvas
+    review_canvas = Canvas(review, bd="3", bg="lightgrey", height=580, width=450)
+    review_canvas.place(relx=.5, rely=.55, anchor=CENTER)
+    review_canvas.config(scrollregion=[0, 0, 450, 1000])
+
+    # Raw Canvas Scrolling Function
+    review_canvas.yview_moveto(1.0)
+
+    # Raw Canvas Scrollbar
+    ybar = Scrollbar(review_canvas, orient=VERTICAL)
+    ybar.place(relx=0, rely=0, height=590, anchor=NW)
+    ybar.config(command=review_canvas.yview)
+    review_canvas.config(yscrollcommand=ybar.set)
 
     review.mainloop()
 
@@ -82,8 +98,8 @@ def open_grouping():
     grouped_label.place(relx=.8, rely=.1, anchor=NE)
 
     # Raw Canvas
-    raw_canvas = Canvas(grouping, bd="3", bg="lightgrey", height=520, width=500)
-    raw_canvas.place(relx=.025, rely=.2, anchor=NW)
+    raw_canvas = Canvas(grouping, bd="3", bg="lightgrey", height=520, width=510)
+    raw_canvas.place(relx=.02, rely=.2, anchor=NW)
     raw_canvas.config(scrollregion=[0, 0, 500, 1000])
 
     # Raw Canvas Scrolling Function
@@ -91,7 +107,7 @@ def open_grouping():
 
     # Raw Canvas Scrollbar
     ybar = Scrollbar(raw_canvas, orient=VERTICAL)
-    ybar.place(relx=0, rely=0, height=520, anchor = NW)
+    ybar.place(relx=0, rely=0, height=530, anchor=NW)
     ybar.config(command=raw_canvas.yview)
     raw_canvas.config(yscrollcommand=ybar.set)
 
@@ -99,16 +115,26 @@ def open_grouping():
     house1 = Image.open("house.jpg")
     temp_house1 = house1.resize((235, 145), Image.LANCZOS)
     re_house1 = ImageTk.PhotoImage(temp_house1)
-    raw_canvas.create_image(10, 10, anchor=NW, image=re_house1)
+    raw_canvas.create_image(20, 10, anchor=NW, image=re_house1)
 
     house2 = Image.open("house.png")
     temp_house2 = house2.resize((235, 145), Image.LANCZOS)
     re_house2 = ImageTk.PhotoImage(temp_house2)
-    raw_canvas.create_image(255, 10, anchor=NW, image=re_house2)
+    raw_canvas.create_image(265, 10, anchor=NW, image=re_house2)
 
     # Grouped Canvas
-    grouped_canvas = Canvas(grouping, bd="3", bg="lightgrey", width=500, height=520)
+    grouped_canvas = Canvas(grouping, bd="3", bg="lightgrey", height=520, width=510)
     grouped_canvas.place(relx=.975, rely=.2, anchor=NE)
+    grouped_canvas.config(scrollregion=[0, 0, 500, 1000])
+
+    # Raw Canvas Scrolling Function
+    grouped_canvas.yview_moveto(1.0)
+
+    # Raw Canvas Scrollbar
+    ybar = Scrollbar(grouped_canvas, orient=VERTICAL)
+    ybar.place(relx=0, rely=0, height=530, anchor=NW)
+    ybar.config(command=grouped_canvas.yview)
+    grouped_canvas.config(yscrollcommand=ybar.set)
 
     grouping.mainloop()
 
@@ -141,3 +167,4 @@ def open_landing():
 open_landing()
 
 # ______________________________________________________________________________________________________________________
+
