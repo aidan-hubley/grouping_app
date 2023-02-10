433f287b95d419efbc2ca3c08ffd5fc49b807154
jp1478 <jp1478@messiah.edu>
2023-02-10 11:05:20 -0500

    Test file change

--- test.py
+++ test.py
@@ -1,4 +1,10 @@
-groups = [['a', 'b'], ['b', 'c'], ['a', 'c']]
-for group in groups:
-    for letter in group:
-        print(letter)
+# groups = [['a', 'b'], ['b', 'c'], ['a', 'c']]
+# for group in groups:
+#     for letter in group:
+#         print(letter)
+
+
+list = ['a', 'b', 'c']
+print(list)
+del list[0]
+print(list)
